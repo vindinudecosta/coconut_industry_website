c14a03deef010e286dc4c7df4b5b3f8f3343ac6c
kenuka de costa <vindinudeco@gmail.com>
2022-11-26 21:30:10 +0530

    commit 11

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,21 +1,4 @@
-<?php include('../includes/connect.php');
-
-function getIPAddress()
-{
-    //whether ip is from the share internet  
-    if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-        $ip = $_SERVER['HTTP_CLIENT_IP'];
-    }
-    //whether ip is from the proxy  
-    elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-    }
-    //whether ip is from the remote address  
-    else {
-        $ip = $_SERVER['REMOTE_ADDR'];
-    }
-    return $ip;
-} ?>
+<?php include('../includes/connect.php'); ?>
 <?php include('../includes/header.php') ?>
 <?php include('../includes/navbar.php') ?>
 
